336c5c60cdd7fd506c7c69a62dd416b4039c81b5
Alexander.Budnitskiy <Alexander.Budnitskiy@dataart.com>
2021-09-08 09:13:44 +0300

    section #6. 66: API => sorting

--- Core/Specifications/ProductWithFilterForCountSpecification.cs
+++ Core/Specifications/ProductWithFilterForCountSpecification.cs
@@ -4,7 +4,9 @@ namespace Core.Specifications
 {
     public class ProductWithFilterForCountSpecification : BaseSpecification<Product>
     {
-        public ProductWithFilterForCountSpecification(ProductSpecParams productParams) : base(x => 
+        public ProductWithFilterForCountSpecification(ProductSpecParams productParams) : base(x =>
+            (string.IsNullOrEmpty(productParams.Search) || x.Name.ToLower().Contains
+                (productParams.Search)) &&
             (!productParams.BrandId.HasValue || x.ProductBrandId == productParams.BrandId) &&
             (!productParams.TypeId.HasValue || x.ProductTypeId == productParams.TypeId)
         )
