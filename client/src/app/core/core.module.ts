c0603aa6d62ded5c62a6001fff8fcd6a42dbe4b1
Alexander.Budnitskiy <Alexander.Budnitskiy@dataart.com>
2021-09-10 15:26:20 +0300

    S-12125. Client => Add breadcrumbs

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { RouterModule } from '@angular/router';
+
+import { BreadcrumbModule } from 'xng-breadcrumb';
+import { ToastrModule } from 'ngx-toastr';
+
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { TestErrorComponent } from './test-error/test-error.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { ServerErrorComponent } from './server-error/server-error.component';
-import { ToastrModule } from 'ngx-toastr';
 import { SectionHeaderComponent } from './section-header/section-header.component';
+
 
 @NgModule({
   declarations: [
@@ -19,6 +23,7 @@ import { SectionHeaderComponent } from './section-header/section-header.componen
   imports: [
     CommonModule,
     RouterModule,
+    BreadcrumbModule,
     ToastrModule.forRoot({
       positionClass: 'toast-bottom-right',
       preventDuplicates: true
