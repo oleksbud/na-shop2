65096fcb4edbe12dd77e35c979b1274c2a637c62
Alexander.Budnitskiy <Alexander.Budnitskiy@dataart.com>
2021-09-17 14:44:19 +0300

    s-15166-fix. API => Migration of identity fix

--- Infrastructure/Identity/AppIdentityDbContextSeed.cs
+++ Infrastructure/Identity/AppIdentityDbContextSeed.cs
@@ -24,7 +24,8 @@ namespace Infrastructure.Identity
                         State = "NY",
                         ZipCode = "90210"
                     }
-                }
+                };
+                await userManager.CreateAsync(user, "Pa$$w0rd");
             }
         }
     }
